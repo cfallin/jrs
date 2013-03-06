03a85932af88bc149d71989cd947e1329304b57a
Chris Fallin <cfallin@c1f.net>
2013-03-05 19:23:15 -0500

    Fixed Debian .deb build script.

--- build-deb.sh
+++ build-deb.sh
@@ -6,11 +6,14 @@ dir=`mktemp -d`
 
 cd $dir
 cp -R $wd $dir/jrs-$vers
-tar jcvf jrs_$vers.orig.tar.bz2 --exclude ".*" jrs-$vers/
+pushd $dir/jrs-$vers
+rm -rf .git* deb/
+popd
+tar jcvf jrs_$vers.orig.tar.bz2 jrs-$vers/
 cd jrs-$vers
 debuild
 cd ..
-mkdir $wd/deb/
+mkdir -p $wd/deb/
 cp jrs* $wd/deb/
 cd $wd
 rm -rf $dir
